edbefb961ddd00300378a5a57571eafb378e6d44
Hyunbok Lee <hyunbogi@gmail.com>
2016-03-29 14:31:18 +0900

    Initial Commit

---
+++ OpenGLSample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLSample
+//
+//  Created by Hyunbok Lee on 2016. 3. 29..
+//  Copyright © 2016년 Hyunbok Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
