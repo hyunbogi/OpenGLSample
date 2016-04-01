ca7bb87ff09fa476d12eec72ed02908b3f326168
Hyunbok Lee <hyunbogi@gmail.com>
2016-04-01 21:08:55 +0900

    Initial commit

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -6,10 +6,102 @@
 //  Copyright © 2016년 Hyunbok Lee. All rights reserved.
 //
 
-#include <iostream>
+#include <math.h>
+#include "sb6.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+/**
+ * Function Prototypes
+ */
+GLuint compileShader();
+
+
+/**
+ * Main Rendering Application
+ */
+class SampleApplication : public sb6::application
+{
+public:
+    void startup()
+    {
+        renderingProgram = compileShader();
+        glGenVertexArrays(1, &vertexArrayObject);
+        glBindVertexArray(vertexArrayObject);
+    }
+
+    void shutdown()
+    {
+        glDeleteVertexArrays(1, &vertexArrayObject);
+        glDeleteProgram(renderingProgram);
+        glDeleteVertexArrays(1, &vertexArrayObject);
+    }
+
+    void render(double currentTime)
+    {
+//        const GLfloat color[] = {
+//                (float) sin(currentTime) * 0.5f + 0.5f,
+//                (float) cos(currentTime) * 0.5f + 0.5f,
+//                0.0f,
+//                1.0f
+//        };
+        const GLfloat color[] = {0.0f, 0.0f, 0.0f, 1.0f};
+
+        glClearBufferfv(GL_COLOR, 0, color);
+
+        glUseProgram(renderingProgram);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+private:
+    GLuint renderingProgram;
+    GLuint vertexArrayObject;
+};
+
+
+DECLARE_MAIN(SampleApplication);
+
+
+GLuint compileShader()
+{
+    static const char *vertexShaderCode[] = {"#version 410 core         \n"
+            "void main(void)                                            \n"
+            "{                                                          \n"
+            "   const vec4 vertices[3] = vec4[3]                        \n"
+            "   (                                                       \n"
+            "       vec4(0.25, -0.25, 0.5, 1.0),                        \n"
+            "       vec4(-0.25, -0.25, 0.5, 1.0),                       \n"
+            "       vec4(0.25, 0.25, 0.5, 1.0)                          \n"
+            "   );                                                      \n"
+            "                                                           \n"
+            "   gl_Position = vertices[gl_VertexID];                    \n"
+            "}                                                          \n"
+    };
+
+    static const char *fragmentShaderCode[] = {"#version 410 core       \n"
+            "out vec4 color;                                            \n"
+            "                                                           \n"
+            "void main(void)                                            \n"
+            "{                                                          \n"
+            "   color = vec4(0.0, 0.8, 1.0, 1.0);                       \n"
+            "}                                                          \n"
+    };
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, vertexShaderCode, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, fragmentShaderCode, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
 }
